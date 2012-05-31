37155342df9ca3d5a49a2c3f3a07e250d06237b1
Martin Lippert <lippert@acm.org>
2012-05-31 16:10:04 +0200

    removed features that got contributed to spring ide and added them back as feature import

--- plugins/com.springsource.sts.ide.help/src/com/springsource/sts/internal/ide/help/SampleProjectsLabelProvider.java
+++
@@ -1,7 +0,0 @@
-package com.springsource.sts.internal.ide.help;
-
-import org.eclipse.jface.viewers.LabelProvider;
-
-public class SampleProjectsLabelProvider extends LabelProvider {
-
-}
--- plugins/com.springsource.sts.wizard/src/com/springsource/sts/wizard/template/infrastructure/ITemplateElement.java
+++
@@ -1,7 +0,0 @@
-package com.springsource.sts.wizard.template.infrastructure;
-
-public interface ITemplateElement {
-
-	public String getName();
-
-}
