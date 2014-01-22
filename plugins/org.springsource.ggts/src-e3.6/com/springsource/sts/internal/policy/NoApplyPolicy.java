6143abc4323721a59154e1fa81b77cf31a6ddd12
Martin Lippert <mlippert@gopivotal.com>
2014-01-22 11:26:02 +0100

    updated company legal name to Pivotal Software Inc.

--- plugins/org.springsource.ggts/src-e3.6/com/springsource/sts/internal/policy/NoApplyPolicy.java
+++ plugins/org.springsource.ggts/src-e3.6/com/springsource/sts/internal/policy/NoApplyPolicy.java
@@ -1,5 +1,5 @@
 /******************************************************************************************
- * Copyright (c) 2010 - 2011 GoPivotal, Inc. All rights reserved.
+ * Copyright (c) 2010 - 2011 Pivotal Software, Inc. All rights reserved.
  ******************************************************************************************/
 package com.springsource.sts.internal.policy;
 
