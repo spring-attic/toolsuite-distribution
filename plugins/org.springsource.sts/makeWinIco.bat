a57d1c6c500cb7ba73b7e8608d5e71406999eada
Kris De Volder <kdevolder@gopivotal.com>
2014-01-28 11:36:51 -0800

    Add sts.ico created with imagemagick on windows
    Add unix version of win icon script
    (Note: only for reference, doesn't actually work)

---
+++ plugins/org.springsource.sts/makeWinIco.bat
@@ -0,0 +1,23 @@
+REM Generates windows .ico file using 'imagemagick' commandline tool.
+REM While this tool is really a unix tool a version of it is availabe for windows.
+REM This bat file needs to be run on a windows system.
+REM Similar command should also work on Linux but for
+REM some reason the version of imagemagick I had on Linux refused to store
+REM The 256x256 image uncompressed.
+REM See http://www.imagemagick.org/discourse-server/viewtopic.php?f=1&t=24883
+
+convert sts256.png -compress none ^
+    "(" -clone 0 -resize 16x16 -compress none ")" ^
+    "(" -clone 0 -resize 24x24 -compress none ")" ^
+    "(" -clone 0 -resize 32x32 -compress none ")" ^
+    "(" -clone 0 -resize 48x48 -compress none ")" ^
+    "(" -clone 0 -resize 16x16 -colors 256 -compress none ")" ^
+    "(" -clone 0 -resize 24x24 -colors 256 -compress none ")" ^
+    "(" -clone 0 -resize 32x32 -colors 256 -compress none ")" ^
+    "(" -clone 0 -resize 48x48 -colors 256 -compress none ")" ^
+    "(" -clone 0 -resize 256x256 -compress none ")" ^
+    -delete 0 sts.ico
+
+REM Below are some useful commands to verify the output, uncomment them to use
+REM identify sts.ico
+REM identify -verbose sts.ico > sts.ico.info
