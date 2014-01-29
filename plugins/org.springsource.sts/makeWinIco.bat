840d1ae6a92e0c5089163e51d982568f68776643
Kris De Volder <kdevolder@gopivotal.com>
2014-01-29 10:33:12 -0800

    Remove 24 bit image from makeWinIco bat script

--- plugins/org.springsource.sts/makeWinIco.bat
+++ plugins/org.springsource.sts/makeWinIco.bat
@@ -8,11 +8,9 @@ REM See http://www.imagemagick.org/discourse-server/viewtopic.php?f=1&t=24883
 
 convert sts256.png -compress none ^
     "(" -clone 0 -resize 16x16 -compress none ")" ^
-    "(" -clone 0 -resize 24x24 -compress none ")" ^
     "(" -clone 0 -resize 32x32 -compress none ")" ^
     "(" -clone 0 -resize 48x48 -compress none ")" ^
     "(" -clone 0 -resize 16x16 -colors 256 -compress none ")" ^
-    "(" -clone 0 -resize 24x24 -colors 256 -compress none ")" ^
     "(" -clone 0 -resize 32x32 -colors 256 -compress none ")" ^
     "(" -clone 0 -resize 48x48 -colors 256 -compress none ")" ^
     "(" -clone 0 -resize 256x256 -compress none ")" ^
