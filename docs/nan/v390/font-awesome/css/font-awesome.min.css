e35c8600f3b36b52bbf06ac98e91c3d8f126f6ca
Martin Lippert <mlippert@pivotal.io>
2017-06-27 13:49:16 +0200

    added first draft for 3.9.0 n&n doc

---
+++ docs/nan/v390/font-awesome/css/font-awesome.min.css
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html xmlns:app="http://www.springframework.org"><head>
+  <title>Page Not Found</title>
+  <link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.css" />
+  <!-- <link rel="stylesheet" type="text/css" th:href="@{/bootstrap/css/bootstrap-responsive.css}" /> -->
+  <link href="http://fonts.googleapis.com/css?family=Varela+Round|Montserrat:400,700" rel="stylesheet" type="text/css" />
+  <link rel="stylesheet" type="text/css" href="/font-awesome/css/font-awesome.min.css" />
+  <link rel="stylesheet" type="text/css" href="/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" />
+
+  <link rel="stylesheet" type="text/css" href="/css/icons.css.css" />
+  <link rel="stylesheet" type="text/css" href="/css/buttons.css.css" />
+  <link rel="stylesheet" type="text/css" href="/css/typography.css.css" />
+
+  <link rel="stylesheet" type="text/css" href="/css/main.css.css" />
+  <link rel="stylesheet" type="text/css" href="/css/application.css.css" />
+  <link rel="stylesheet" type="text/css" href="/css/guide.css.css" />
+  <link rel="stylesheet" type="text/css" href="/css/team.css.css" />
+  <link rel="stylesheet" type="text/css" href="/css/docs.css.css" />
+  <link rel="stylesheet" type="text/css" href="/css/homepage.css.css" />
+  <link rel="stylesheet" type="text/css" href="/css/tools.css.css" />
+  <link rel="stylesheet" type="text/css" href="/css/projects.css.css" />
+  <link rel="stylesheet" type="text/css" href="/css/highlight.css.css" />
+  
+
+  <script type="text/javascript" src="/js/jquery.js"></script>
+  <script type="text/javascript" src="/bootstrap/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="/js/ZeroClipboard.min.js"></script>
+  <script type="text/javascript" src="/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js"></script>
+
+  <script type="text/javascript" src="/js/filter.js"></script>
+  <script type="text/javascript" src="/js/application.js"></script>
+  <script type="text/javascript" src="/js/guide.js"></script>
+  <meta content="7qGntFPD9lWAVCtUu5U77v4l68PsTHf6xpzgjQv2j2M" name="google-site-verification" />
+  <script>
+    //
+      var _gaq = _gaq || [];
+      var pluginUrl = 
+       '//www.google-analytics.com/plugins/ga/inpage_linkid.js';        
+      _gaq.push(
+        ['_require', 'inpage_linkid', pluginUrl],
+        ['_setAccount', 'UA-2728886-20'],
+        ['_trackPageview'],
+        ['b._require', 'inpage_linkid', pluginUrl],
+        ['b._setAccount', 'UA-2728886-19'],
+        ['b._trackPageview']
+      );
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+  </script>
+
+	
+</head><body onload="checkCookie()">
+  <div class="viewport">
+    <header class="navbar header--navbar header-web">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <div class="spring-logo--container">
+            <div class="logo-bg"></div>
+            <a class="spring-logo" href="/"></a>
+          </div>
+          <ul class="nav pull-right">
+            <li class="navbar-link">
+              <a href="/docs">Docs</a>
+            </li>
+            <li class="navbar-link">
+              <a href="/guides">Guides</a>
+            </li>
+            <li class="navbar-link">
+              <a href="/projects">Projects</a>
+            </li>
+            <li class="navbar-link">
+              <a href="/blog">Blog</a>
+            </li>
+            <li class="navbar-link">
+              <a href="http://forum.springframework.io">Forum</a>
+            </li>
+            <li class="navbar-link nav-search">
+              <i class="icon-search navbar-search--icon js-search-input-open"></i>
+              <div class="search-input-close js-search-input-close">x</div>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="search-dropdown--container js-search-dropdown">
+        <div class="container-fluid">
+          <div class="search-form--container">
+            <form action="/search" class="form-inline form-search" method="get">
+              <input class="search-query search-form--form js-search-input" name="q" placeholder="Search for documentation, guides, and posts..." type="text" value="" />
+              <button class="search-form--submit" type="submit"><i class="icon-search"></i></button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div class="header--container">
+      <div class="header--content">
+        <div class="row-fluid container-fluid">
+          <div class="span8">
+            <h1 class="header--content-title">Page Not Found</h1>
+            <p class="header--content-subtitle">
+              Subttitle Ligula Risus Tristique Pharetra
+            </p>
+          </div>
+          <div class="span4">
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="body--container container-fluid ">
+	404. Sorry.<br />
+	<img src="http://ithassoul.com/data/published/05_2011/f1ca80c5e09dcc28cf4912b951a4aa50_pub.jpg" />
+</div>
+
+    <footer class="footer">
+      <div class="container-fluid">
+        <div class="row-fluid">
+          <div class="span8">
+            <div class="navbar">
+              <div class="container">
+                <ul class="nav">
+                  <li><a href="/tools">Tools</a></li>
+                  <li><a href="/services">Services</a></li>
+                  <li><a href="/about">About Us</a></li>
+                  <li><a href="/jobs">Work With Us</a></li>
+                </ul>
+              </div>
+            </div>
+            &copy; <script type="text/javascript"> var d = new Date();
+                  document.write(d.getFullYear());</script> GoPivotal, Inc. All Rights Reserved.
+            <a href="http://www.gopivotal.com/terms-of-use">Terms</a>,
+            <a href="http://www.gopivotal.com/privacy-policy">Privacy</a>, and
+            <a href="/logos">Usage</a>
+            
+          </div>
+          <div class="span4 footer-newsletter--wrapper pull-right">
+            <div class="footer-newsletter--container">
+              <label>
+                Subscribe to our newsletter
+              </label>
+              <form action="http://play.gopivotal.com/index.php/leadCapture/save" class="lpeRegForm formNotEmpty" enctype="application/x-www-form-urlencoded" id="mktForm_1035" method="post" name="mktForm_1035">
+                <ul class="mktLblLeft" style="margin: 0;">
+                  <li class="mktField">
+                    <span class="mktInput">
+                      <input class="mktFormText mktFormEmail" id="Email" maxlength="255" name="Email" placeholder="Email Address" style="font-size: 12px; padding: 10px 0 27px 10px; height:21px; font-family: &quot;Varela Round&quot;, sans-serif; color: #666; font-weight: 400;border-radius: 0;width: 310px;" tabindex="1" title="Email Address" type="text" value="" />
+                      <span class="mktFormMsg"></span>
+                    </span>
+                  </li>
+                  <li class="mktField" style="display: none;">
+                    <span class="mktInput">
+                      <input class="mktFormHidden" id="OSS__c" name="OSS__c" type="hidden" value="Yes" />
+                      <span class="mktFormMsg"></span>
+                    </span>
+                  </li>
+                  <li class="mktField" style="display: none;">
+                    <span class="mktInput">
+                      <input class="mktFormHidden" id="Primary_Product_Interest__c" name="Primary_Product_Interest__c" type="hidden" value="OSS" />
+                      <span class="mktFormMsg"></span>
+                    </span>
+                  </li>
+                  <li class="mktField" style="display: none;">
+                    <span class="mktInput">
+                      <input class="mktFormHidden" id="Original_Lead_Source__c" name="Original_Lead_Source__c" type="hidden" value="Website" />
+                      <span class="mktFormMsg"></span>
+                    </span>
+                  </li>
+                  <li class="mktField" style="display: none;">
+                    <span class="mktInput">
+                      <input class="mktFormHidden" id="Original_Lead_Source_Description__c" name="Original_Lead_Source_Description__c" type="hidden" value="OSS_Website_SpringNewsletterSubscription" />
+                      <span class="mktFormMsg"></span>
+                    </span>
+                  </li>
+                  <li class="mktField" style="display: none;">
+                    <span class="mktInput">
+                      <input class="mktFormHidden" id="LeadSource" name="LeadSource" type="hidden" value="Website" /><span class="mktFormMsg"></span>
+                    </span>
+                  </li>
+                  <li class="mktField" style="display: none;">
+                    <span class="mktInput">
+                      <input class="mktFormHidden" id="Most_Recent_Lead_Source_Description__c" name="Most_Recent_Lead_Source_Description__c" type="hidden" value="OSS_Website_SpringNewsletterSubscription" />
+                      <span class="mktFormMsg"></span>
+                    </span>
+                  </li>
+                  <li id="mktFrmButtons">
+                    <input id="mktFrmSubmit" name="submitButton" onclick="setSubscribeCookie(); formSubmit(document.getElementById(&quot;mktForm_1035&quot;)); return false;" type="submit" value="Subscribe" />&nbsp;<input id="mktFrmReset" name="resetButton" onclick="formReset(document.getElementById(&quot;mktForm_1035&quot;));" style="display: none;" type="reset" value="Clear" />
+                  </li>  
+                </ul>
+                <span style="display:none;">
+                  <input name="_marketo_comments" type="text" value="" />
+                </span>
+                <input name="lpId" type="hidden" value="-1" />
+                <input name="subId" type="hidden" value="137" />
+                <input name="munchkinId" type="hidden" value="625-IUJ-009" />
+                <input name="kw" type="hidden" value="" />
+                <input name="cr" type="hidden" value="" />
+                <input name="searchstr" type="hidden" value="" />
+                <input name="lpurl" type="hidden" value="http://play.gopivotal.com/OSS_Website_Spring_SpringNewsletterSubscriptionEmailOnly.html?cr={creative}&amp;kw={keyword}" />
+                <input name="formid" type="hidden" value="1035" />
+                <input name="returnURL" type="hidden" value="" />
+                <input name="retURL" type="hidden" value="" />
+                <input name="returnLPId" type="hidden" value="-1" />
+                <input name="_mkt_disp" type="hidden" value="return" />
+                <input name="_mkt_trk" type="hidden" value="" />
+              </form>
+              <script src="http://play.gopivotal.com/js/mktFormSupport.js" type="text/javascript"></script>
+              <script type="text/javascript">
+              function formSubmit(elt) {
+                return Mkto.formSubmit(elt);
+              }
+              function formReset(elt) {
+                return Mkto.formReset(elt);
+              }
+              </script>
+            </div>
+          </div>
+        </div>
+      </div>    
+    </footer>
+    <div id="scrim"></div>
+  </div>
+  <script type="text/javascript" src="/js/marketomunchkin.js"></script>
+
+</body></html>
